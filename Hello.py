ce7d56c6cc8367d27288f326955a65d74ed3933a
Birdyyyyy <632676124@qq.com>
2020-05-16 00:24:41 +0800

    a new file created by Zhang

---
+++ Hello.py
@@ -0,0 +1,2 @@
+print("new file pushed by Yuan")
+print("hello Zhang!!")
---
+++ Star.py
@@ -0,0 +1,12 @@
+from turtle import *
+setup(450,450)
+pensize(10)
+penup()
+seth(-30)
+pendown()
+color("yellow")
+for i in range(5):
+    fd(50)
+    seth(-30)
+    fd(50)
+    seth(60)
